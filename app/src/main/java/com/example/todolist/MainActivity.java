a4cfca1365ae70198c362532b0d134ead1c7e4d3
Mai Hieu <maixuanhieuytt@gmail.com>
2025-05-11 18:27:39 +0700

    update

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.todolist.fragment.FragmentCalender;
+import com.example.todolist.fragment.FragmentHome;
+import com.example.todolist.fragment.FragmentProfile;
+import com.example.todolist.fragment.FragmentStatistics;
+import com.example.todolist.fragment.FragmentTask;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager manager;
+    FragmentTransaction transaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        manager = getSupportFragmentManager();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            String tag = null;
+
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                selectedFragment = new FragmentHome();
+                tag = "HOME";
+            } else if (id == R.id.task) {
+                selectedFragment = new FragmentTask();
+                tag = "TASK";
+            } else if (id == R.id.statistics) {
+                selectedFragment = new FragmentStatistics();
+                tag = "STATISTICS";
+            } else if (id == R.id.calender) {
+                selectedFragment = new FragmentCalender();
+                tag = "CALENDER";
+            } else if (id == R.id.profile) {
+                selectedFragment = new FragmentProfile();
+                tag = "PROFILE";
+            }
+
+            if (selectedFragment != null) {
+                add(selectedFragment, tag, tag);
+                return true;
+            }
+
+            return false;
         });
+
+
+    }
+
+    private void add(Fragment fg, String tag, String name) {
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fg, tag); // dùng replace để tránh chồng fragment
+        transaction.addToBackStack(name);
+        transaction.commit();
+    }
+    @Override
+    public void onBackPressed() {
+        if (manager.getBackStackEntryCount() > 0) {
+            manager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
