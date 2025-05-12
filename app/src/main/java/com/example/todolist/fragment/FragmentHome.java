f692b1bca161fa0c1f4d342889adf29110453a5f
ngantran13 <nganguyetkimthi@gmail.com>
2025-05-13 01:30:48 +0700

    insert dem nguoc

--- app/src/main/java/com/example/todolist/fragment/FragmentHome.java
+++ app/src/main/java/com/example/todolist/fragment/FragmentHome.java
@@ -1,5 +1,6 @@
 package com.example.todolist.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.example.todolist.DemActivity;
 import com.example.todolist.R;
 
 public class FragmentHome extends Fragment {
+
+    CardView cardCountdown;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,5 +26,13 @@ public class FragmentHome extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        cardCountdown=view.findViewById(R.id.cardCountdown);
+        cardCountdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(requireContext(), DemActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
